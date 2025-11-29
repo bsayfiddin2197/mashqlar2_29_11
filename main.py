7aea0a1583b3d57405e8e49c6eeeedc916cccb88
bsayfiddin2197 <bsayfiddin2197@gmail.com>
2025-11-29 17:12:42 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# 6
+uzun2 = lambda srt: len(srt)
+
+res = uzun2("Sayfiddin")
+print(res)
+
+# 7
+mmfy = lambda son: "musbat" if son > 0 else ("nol" if son == 0 else "manfiy")
+
+
+res = mmfy(7)
+print(res)
+
+# 8
+ucbrchak = lambda asos, blndlk: 0.5 * asos * blndlk
+
+res = ucbrchak(12, 34)
+print(res)
+
+# 9
+juftmi = lambda x: x % 2 == 0
+
+res = juftmi(7)
+print(res)
+
+# 10
+katta2 = lambda x: x > 10
+
+res = katta2(11)
+print(res)
